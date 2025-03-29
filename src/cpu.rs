75917a3a7d7ca0574cf479b1b405e60f59c9d226
suryxks <suryasuresh9876543@gmail.com>
2025-03-29 17:05:39 +0530

    feat:ADD,SUB and CMP with sign and zero flags

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,10 @@
+use core::net;
+
 use crate::instruction::{ Instruction, Operand, Operation, Register };
 #[derive(Debug, Clone, Copy)]
 pub struct Cpu {
     pub registers: CPURegisters,
+    pub flags: Flags,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -14,6 +17,88 @@ pub struct CPURegisters {
     pub bp: u16,
     pub si: u16,
     pub di: u16,
+}
+pub enum Flag {
+    CF,
+    PF,
+    AF,
+    ZF,
+    SF,
+    OF,
+}
+#[derive(Debug, Clone, Copy)]
+pub struct Flags {
+    pub cf: bool, //carry
+    pub pf: bool, // parity
+    pub af: bool, // auxilary carry
+    pub zf: bool, //zero
+    pub sf: bool, //sign
+    pub of: bool, //overflow
+}
+impl Flags {
+    pub fn new() -> Self {
+        Flags {
+            cf: false,
+            pf: false,
+            af: false,
+            zf: false,
+            sf: false,
+            of: false,
+        }
+    }
+    pub fn set(&mut self, flag: Flag) {
+        match flag {
+            Flag::AF => {
+                self.af = true;
+            }
+            Flag::CF => {
+                self.cf = true;
+            }
+            Flag::PF => {
+                self.pf = true;
+            }
+            Flag::ZF => {
+                self.zf = true;
+            }
+            Flag::SF => {
+                self.sf = true;
+            }
+            Flag::OF => {
+                self.of = true;
+            }
+        }
+    }
+    pub fn unset(&mut self, flag: Flag) {
+        match flag {
+            Flag::AF => {
+                self.af = false;
+            }
+            Flag::CF => {
+                self.cf = false;
+            }
+            Flag::PF => {
+                self.pf = false;
+            }
+            Flag::ZF => {
+                self.zf = false;
+            }
+            Flag::SF => {
+                self.sf = false;
+            }
+            Flag::OF => {
+                self.of = false;
+            }
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.cf = false;
+        self.pf = false;
+        self.af = false;
+        self.zf = false;
+        self.sf = false;
+        self.of = false;
+    }
 }
 impl CPURegisters {
     pub fn new() -> Self {
@@ -28,7 +113,7 @@ impl CPURegisters {
             di: 0,
         }
     }
-    pub fn get(&self, reg: Register) -> u16 {
+    pub fn get(&self, reg: &Register) -> u16 {
         match reg {
             Register::AH => self.ax[0] as u16,
             Register::AL => self.ax[1] as u16,
@@ -48,7 +133,7 @@ impl CPURegisters {
             Register::SP => self.sp,
         }
     }
-    pub fn set(&mut self, reg: Register, val: u16) {
+    pub fn set(&mut self, reg: &Register, val: u16) {
         match reg {
             Register::AH => {
                 self.ax[0] = val as u8;
@@ -107,7 +192,7 @@ impl CPURegisters {
 }
 impl Cpu {
     pub fn new() -> Self {
-        Cpu { registers: CPURegisters::new() }
+        Cpu { registers: CPURegisters::new(), flags: Flags::new() }
     }
     pub fn execute(&mut self, instruction: &Instruction) {
         match instruction.operation {
@@ -116,11 +201,131 @@ impl Cpu {
                     (Some(source), destination) => {
                         match (source, destination) {
                             (Operand::Register(reg1), Operand::Register(reg2)) => {
-                                self.registers.set(reg2.clone(), self.registers.get(reg1.clone()));
+                                self.registers.set(&reg2, self.registers.get(&reg1));
                             }
                             (Operand::Immediate(val), Operand::Register(reg)) => {
-                                self.registers.set(reg.clone(), *val as u16);
+                                self.registers.set(&reg, *val as u16);
                             }
+                            (_, _) => { println!("Not supported") }
+                        }
+                    }
+                    (_, _) => { println!("Not supported") }
+                }
+            }
+            Operation::Add => {
+                match (&instruction.source, &instruction.destination) {
+                    (Some(source), destination) => {
+                        match (source, destination) {
+                            (Operand::Register(src), Operand::Register(dest)) => {
+                                let src_val = self.registers.get(&src);
+                                let dest_val = self.registers.get(&dest);
+                                let (result, overflowed) = dest_val.overflowing_add(src_val);
+
+                                self.registers.set(&dest, result);
+                                if overflowed {
+                                    self.flags.set(Flag::OF);
+                                }
+                            }
+                            (Operand::Immediate(val), Operand::Register(reg)) => {
+                                let reg_val = self.registers.get(&reg);
+
+                                let (result, overflowed) = reg_val.overflowing_add(*val as u16);
+
+                                self.registers.set(&reg, result);
+                                if overflowed {
+                                    self.flags.set(Flag::OF);
+                                } else {
+                                    self.flags.unset(Flag::OF);
+                                }
+                            }
+
+                            (_, _) => { println!("Not supported") }
+                        }
+                    }
+                    (_, _) => { println!("Not supported") }
+                }
+            }
+            Operation::Sub => {
+                match (&instruction.source, &instruction.destination) {
+                    (Some(source), destination) => {
+                        match (source, destination) {
+                            (Operand::Register(src), Operand::Register(dest)) => {
+                                let src_val = self.registers.get(&src);
+                                let dest_val = self.registers.get(&dest);
+                                let (result, is_negative) = dest_val.overflowing_sub(src_val);
+                                match (result, is_negative) {
+                                    (0, _) => {
+                                        self.flags.set(Flag::ZF);
+                                        self.flags.unset(Flag::SF);
+                                    }
+                                    (_, true) => {
+                                        self.flags.unset(Flag::ZF);
+                                        self.flags.set(Flag::SF);
+                                    }
+                                    (_, _) => {}
+                                }
+
+                                self.registers.set(&dest, result);
+                            }
+                            (Operand::Immediate(val), Operand::Register(reg)) => {
+                                let reg_val = self.registers.get(&reg);
+
+                                let (result, is_negative) = reg_val.overflowing_sub(*val as u16);
+                                match (result, is_negative) {
+                                    (0, _) => {
+                                        self.flags.set(Flag::ZF);
+                                        self.flags.unset(Flag::SF);
+                                    }
+                                    (_, true) => {
+                                        self.flags.set(Flag::SF);
+                                        self.flags.unset(Flag::ZF);
+                                    }
+                                    (_, _) => {}
+                                }
+                                self.registers.set(&reg, result);
+                            }
+
+                            (_, _) => { println!("Not supported") }
+                        }
+                    }
+                    (_, _) => { println!("Not supported") }
+                }
+            }
+            Operation::Cmp => {
+                match (&instruction.source, &instruction.destination) {
+                    (Some(source), destination) => {
+                        match (source, destination) {
+                            (Operand::Register(src), Operand::Register(dest)) => {
+                                let src_val = self.registers.get(&src);
+                                let dest_val = self.registers.get(&dest);
+                                let (result, is_negative) = dest_val.overflowing_sub(src_val);
+                                match (result, is_negative) {
+                                    (0, _) => {
+                                        self.flags.set(Flag::ZF);
+                                        self.flags.unset(Flag::SF);
+                                    }
+                                    (_, true) => {
+                                        self.flags.set(Flag::SF);
+                                        self.flags.unset(Flag::ZF);
+                                    }
+                                    (_, _) => {}
+                                }
+                            }
+                            (Operand::Immediate(val), Operand::Register(reg)) => {
+                                let reg_val = self.registers.get(&reg);
+
+                                let (result, is_negative) = reg_val.overflowing_sub(*val as u16);
+                                match (result, is_negative) {
+                                    (0, _) => {
+                                        self.flags.set(Flag::ZF);
+                                    }
+                                    (_, true) => {
+                                        self.flags.set(Flag::SF);
+                                    }
+                                    (_, _) => {}
+                                }
+                            }
+
                             (_, _) => { println!("Not supported") }
                         }
                     }
